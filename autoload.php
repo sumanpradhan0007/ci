0721ab041ab2597139b63d5c20f5c2b5dc51034c
Suman Pradhan <56202336+sumanpradhan0007@users.noreply.github.com>
2019-11-20 07:57:53 +0530

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php 
+  // load libraries
+  $autoload['libraries'] = array('database', 'session', 'table', 'upload');
+  // load helper
+  $autoload['helper'] = array('url', 'form', 'html', 'date');
+?>
