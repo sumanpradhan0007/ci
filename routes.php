1eb0d60b59d83e6a7f62198dcc45cbeed904b672
Suman Pradhan <56202336+sumanpradhan0007@users.noreply.github.com>
2019-11-20 07:57:13 +0530

    Create routes.php

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php 
+  // create routes path
+  $route['signup'] = 'auth/register';
+  $route['signin'] = 'auth/login';
+  $route['profile'] = 'auth/index';
+  $route['setting'] = 'auth/changepwd';
+  $route['profile/edit'] = 'auth/edit';
+  $route['forgotpwd'] = 'auth/forgotpassword';
+?>
